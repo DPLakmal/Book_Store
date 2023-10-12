e56768f7f573968e57894027d6197c9d5d0fba9e
testing doonly <106380029+DPLakmal@users.noreply.github.com>
2023-10-13 00:16:09 +0530

    Create delete a book  by id

--- backend/index.js
+++ backend/index.js
@@ -41,30 +41,44 @@ app.get("/books/:id", async (request, response) => {
 
 // route for update a book
 app.put("/books/:id", async (request, response) => {
+  try {
+    if (
+      !request.body.title ||
+      !request.body.author ||
+      !request.body.publishYear
+    ) {
+      return response.status(400).send({
+        message: "send all required fields: title, author, publishYear",
+      });
+    }
+    const { id } = request.params;
+
+    const result = await Book.findByIdAndUpdate(id, request.body);
+    if (!result) {
+      return response.status(404).send({ message: "book not found" });
+    }
+    return response.status(200).send({ message: "Book updated successfully" });
+  } catch (error) {
+    console.log(error.message);
+    response.status(500).send({ message: error.message });
+  }
+});
+
+
+// route for delete one book form database by id
+app.delete("/books/:id", async (request, response) => {
     try {
-      if (
-        !request.body.title ||
-        !request.body.author ||
-        !request.body.publishYear
-      ) {
-        return response.status(400).send({
-          message: "send all required fields: title, author, publishYear",
-        });
-      }
-    const {id} = request.params;
-  
-      const result = await Book.findByIdAndUpdate(id, request.body);
-      if (!result) {
-        return response.status(404).send({message: 'book not found'})        
-      }
-      return response.status(200).send({message:'Book updated successfully'})
-      return response.status(201).send(book);
+      const { id } = request.params;
+      const result = await Book.findByIdAndDelete(id);
+    if (!result) {
+      return response.status(404).send({ message: "book not found" });
+    }
+      return response.status(200).send({message: `${id} book is deleted successfully`});
     } catch (error) {
       console.log(error.message);
       response.status(500).send({ message: error.message });
     }
   });
-  
 
 // route for save a new book
 app.post("/books", async (request, response) => {
