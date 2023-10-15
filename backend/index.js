3957b99e825a9ce867a4b1f8e3135d40251b4b45
testing doonly <106380029+DPLakmal@users.noreply.github.com>
2023-10-15 19:35:23 +0530

    Update cors configs

--- backend/index.js
+++ backend/index.js
@@ -11,11 +11,19 @@ const app = express();
 //Middleware for parsing request body
 app.use(express.json());
 
-app.use(cors({
-  origin: ' http://localhost:3000',
-  methods:['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type'],
-}))
+// app.use(cors({
+//   origin: ' http://localhost:3000',
+//   methods:['GET', 'POST', 'PUT', 'DELETE'],
+//   allowedHeaders: ['Content-Type'],
+// }))
+
+// Allow requests from 'http://localhost:5173'
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
 
 app.get("/", (request, response) => {
   console.log(request);
